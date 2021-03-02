b82ad3784bb265ac340c8a9b85affb426044d67f
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-03-02 13:09:19 -0300

    Atualização 02/03/2021 Caio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Projeto Integrador
+          Projeto Integrador I
         </a>
       </header>
     </div>
