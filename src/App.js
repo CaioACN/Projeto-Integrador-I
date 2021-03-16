aa23aaba8375126eac66135e1b8901d78fd1c823
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-03-16 16:41:08 -0300

    FormulárioSelect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import './App.css';
-
+import Header from './components/header/header';
+import Routes from './routes';
 import Login from '../src/pages/Login' ;
 
 function App() {
   return (
     <div className="App">
       < Login />
+      < Header />
+      <Routes/>
+
     </div>
   );
 }
 
+
+
+
 export default App;
+
---
+++ src/pages/usuario/main/index.js
@@ -0,0 +1,34 @@
+import React, {component} from 'react';
+import { link } from 'react-router-dom';
+import './index.css';
+
+export class Main extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            usuario: []
+        }
+    }
+    componentDidMount(){
+        fetch('endereçobackend')
+        .then(usuario =>
+            usuario.json().then(usuario => this.setState({usuario}))
+            )
+    }
+    render() {
+        const {usuário} = this.state;
+
+        return usuario.map((usuario,index) => (
+            <div className="usuario-list">
+                <div Key= {index}>
+                    <h5>{usuario.nome}</h5>
+                    <article>
+                    <strong>{usuario.salario}</strong>
+                    <p><link to={'/usuarios/${usuario.id}'}>Detalhes</link></p>
+                    <br/>
+                    </article>
+                    </div>
+            </div>
+        ))
+    }
+}
