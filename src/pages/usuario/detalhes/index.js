1b3a46849582fb04dccf0e97706ada46e723b3f1
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-03-16 18:53:47 -0300

    FormatDetails

---
+++ src/pages/usuario/detalhes/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { link } from 'react-router-dom';
+import './index.css';
+
+export default class Usuario extends Component {
+    state = {
+        usuario: {}
+    };
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+
+        fetch('endereçoBackends/${id}')
+            .then(usuario =>
+                usuario.json().then(usuario => this.setState({ usuario }))
+            )
+    }
+
+    render() {
+        const { usuario } = this.state;
+
+        if(usuario.ativo){
+            usuario.ativo = "Usuário Ativo";
+        }else{
+            usuario.ativo ="Usuário Inativo";
+        }
+
+        return (
+                <div className="usuario-info">
+                    <h1>{usuario.nome}</h1>
+                    <h1>{usuario.ativo}</h1>
+                    <h1>{usuario.salario}</h1>
+                    <h1>{usuario.dataNascimento}</h1>
+                    <br />
+                </div>
+                <Link to={'/usuarios'}>Voltar</Link> <br />
+                <Link to={'/editarusuarios/${usuario.id}'}>Editar</Link><br />
+                <Link to={'/deletarusuarios/${usuario.id}'}>Deletar</Link><br />
+
+        )
+    }
+}
+
