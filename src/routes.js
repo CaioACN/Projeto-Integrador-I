b71eee9f53bf92e790f7a8139221ad6b74451aa5
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-03-18 15:59:28 -0300

    Atualização 18/03/2021

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,14 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import MainUsuario from './pages/usuario/main';
 import DetalhesUsuario from './pages/usuario/detalhes';
-import MainUsuario from './pages/usuario/main';
+
 
 const Routes = () => (
 <BrowserRouter>
     <Switch>
         <Route exact path = "/usuarios" component={Usuario}/>
         <Route path = "/usuarios/:id" component={DetalhesUsuario}/>
-        <Route path = "/usuarios/:id" component={MainUsuario}/>
+        <Route path = "/usuario/:id" component={MainUsuario}/>
     </Switch>
 </BrowserRouter>
 )
